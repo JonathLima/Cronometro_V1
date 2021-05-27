03fb7126ecb932574a1f1d66697f07780643a549
Jonathan Silva <84137692+JonathSilva@users.noreply.github.com>
2021-05-27 01:06:43 -0300

    Update cr.js

--- assets/js/cr.js
+++ assets/js/cr.js
@@ -2,18 +2,18 @@ const btnStart = document.getElementById('start')
 const btnPause = document.getElementById('pause')
 const btnReset = document.getElementById('reset')
 
+let ms = 0;
 let s = 0;
 let m = 0;
-let h = 0;
 
 btnStart.addEventListener("click", () => {
     cron = setInterval(() => {
         s++;
 
-        if (s == 99) { s = 0; m++; }
-        if (m == 60) { m = 0; h++; }
+        if (ms == 99) { ms = 0; s++; }
+        if (s == 60) { s = 0; m++; }
 
-        let format = (h < 10 ? '0' + h : h) + ':' + (m < 10 ? '0' + m : m) + ':' + (s < 10 ? '0' + s : s);
+        let format = (m < 10 ? '0' + m : m) + ':' + (s < 10 ? '0' + s : s) + ':' + (ms < 10 ? '0' + ms : ms);
         document.getElementById('contador').innerText = format;
     }, 10);
 
@@ -25,9 +25,9 @@ btnPause.addEventListener("click", () => {
 
 btnReset.addEventListener("click", () => {
     clearInterval(cron);
+    ms = 0;
     s = 0;
     m = 0;
-    h = 0;
 
     document.getElementById('contador').innerText = '00:00:00';
 })
